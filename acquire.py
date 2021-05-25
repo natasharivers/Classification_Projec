18250b699647152d90dd66e75a0d869feda6556a
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-25 11:47:06 -0500

    create acquire.py

---
+++ acquire.py
@@ -0,0 +1,35 @@
+#import libraries
+import pandas as pd
+import numpy as np
+import os
+from pydataset import data
+
+# acquire
+from env import host, user, password
+
+
+# Create helper function to get the necessary connection url.
+def get_connection(db_name):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    from env import host, user, password
+    return f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
+
+
+#create function to retrieve telco_churn data
+def get_telco_churn_data():
+    '''
+    This function reads in the telco churn data from the Codeup db
+    and returns a pandas DataFrame with all tables joined.
+    '''
+    
+    sql_query = '''
+    SELECT *
+    FROM customers
+    JOIN contract_types ON customers.contract_type_id = contract_types.contract_type_id
+    JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id
+    JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
+    '''
+    return pd.read_sql(sql_query, get_connection('telco_churn'))
